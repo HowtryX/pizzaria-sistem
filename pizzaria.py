42fb22bab7cf8685cc1e93fb90d697400309385c
HowtryX <thyworaf.onsox@gmail.com>
2026-03-07 21:38:26 -0300

    Update pizzaria.py

--- pizzaria.py
+++ pizzaria.py
@@ -75,7 +75,7 @@ aba = st.sidebar.radio("Navegação:", ["PDV - Pedidos", "Cardápio", "Promoçõ
 if aba == "PDV - Pedidos":
   st.header("🛒 Terminal de Vendas")
 
-    # 1. Busca de Cliente
+  # 1. Busca de Cliente
 nome_busca = st.text_input("🔍 Buscar cliente:")
 resultados = [c for c in st.session_state.clientes if nome_busca.lower() in c.get('nome', '').lower()]
 
@@ -376,6 +376,7 @@ elif aba == "Promoções":
 elif aba == "Relatório":
     st.header("📊 Vendas")
     st.dataframe(pd.DataFrame(st.session_state.vendas))
+
 
 
 
